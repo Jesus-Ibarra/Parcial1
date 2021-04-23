808086e8059f09b5c9e1e9987e9268acf169a3a5
Jesus-Ibarra <jesus.ibarra@udea.edu.co>
2021-04-22 22:17:38 -0500

    codigo casi terminado

--- Codigo2.ino
+++ Codigo2.ino
@@ -1,114 +1,141 @@
-//Serial para las 8 filas :7
-//Timer de salida para todo: 6
-//Serial para asignar la fila: 2
-//Timer para asigar la fila:3
-//Timer por columna:5
-int verifi(int arr[8][8]);
-void iMPatron(int arreg[8][8]);
-
 const int serFila =2;
 const int Rfil =3;
 const int Rsalcol =4;
 const int RCol =5;
 const int RSalfil=6;
+int clear[8][8];
 
-int main(){
-
-  // Inicializacion de puertos
+void setup(){
   for(int pin = 2; pin<7;pin++){
-  	 pinMode(pin, OUTPUT);
+     pinMode(pin, OUTPUT);
   }
-  Serial.begin(9600);
-  
-  
-  byte arr[8]={0xFF,0x7F,0x3F,0x1F,0xF,0x7,0x3,0x1};
-  byte arr2[8]={0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
-  int patron[8][8];
-	bool ps =false;
+    Serial.begin(9600);
+  for(int col=0;col<8;col++){for(int fil=0;fil<8;fil++){clear[col][fil]=0;}}
+}
+
+
+void loop(){
+  int mat[8][8];
+  for(int col=0;col<8;col++){for(int fil=0;fil<8;fil++){mat[col][fil]=0;}}
+  	int op;
+  /*------ Menu ------*/
+  op = Menu();
+  switch(op){
+    case(1):{verifi();}
+    break;
     
-  while(true){
-    for(int i = 0; i <8;i++){
-      for(int bi =0; bi <8;bi++){
-        if(ps){patron[i][bi]= GetBit(arr[i],bi);}
-        else{patron[i][bi]= GetBit(arr[i],7-bi);}
-        Serial.print(patron[i][bi]);
-      }
-      Serial.println();
+    case(2):{
+      imagen(&mat[0][0]);
+      iMPatron(mat);
+      Serial.println("Presione enter para continuar");
+      IngresarDa();
+      iMPatron(clear);
     }
-    Serial.println();
-    iMPatron(patron);
-
-    if(ps){ps=false;}
-    else{ps=true;}
+    break;
+    
+    case(3):{publik();}
+    break;
   }
-  }
-bool GetBit( byte N, int pos)
-   {                 // pos = 7 6 5 4 3 2 1 0
-       int b = N >> pos ;         // Shift bits
-       b = b & 1 ;                // coger solo el ultimo bit
-       return b ;
-   }
-int verifi(int arr[8][8]){
+  iMPatron(clear);
+}
 
-    int i=0;
-    int j=0;
-    for (int i=0; i<8; i++){//Recorre todas las posiciones de las filas
-        for (int j=0;j<8;j++){  //Recorre todas las posiciones de las columnas
-            	//cout<<"Digite un numero ["<<f<<"]["<<c<<"] :";
-            	//cin>>arreg[f][c];
-            }
-        }
-    for (int i=0;i<8;i++){ //Imprime la matriz con los led prendidos
-        for(int j=0;j<8;j++){
-            //cout<<arr[i][j];
-        }
-        //cout<<'\n';
-    }
-    return arr[i][j];
+
+
+
+int Menu(){
+  int op;
+  Serial.println("------ Menu ------");
+  Serial.println("1 -> Verificacion de funcionamiento");
+  Serial.println("2 -> Ingreso de un patron e impresion del mismo");
+  Serial.println("3 -> Impresion de una secuensia de patrones ingresados");
+  Serial.println();
+  Serial.print("Ingrese su opcion: ");
+  op = IngresarDa();
+  Serial.println(op);
+  return op;
+}
+
+void verifi(){
+  int patron[8][8];
+  for(int col=0;col<8;col++){for(int fil=0;fil<8;fil++){patron[col][fil]=1;}}
+  iMPatron(patron);
 }
 
 void publik(){
-//# p
+  //falta terminar.
   int p;
-  int patrones[p][8][8];
-  //Ingreso por el serial
-  for (int patron = 0;patron <p;p++){
-  	//patrones[patron] = verifi()
-  } 
-}
-void iMPatron(int arreg[8][8]){
-  /*
-  for(int c=0;c<8;c++){
-    for(int f =0; f<8;f++){
-      digitalWrite(serFila,arreg[f][c]);
-      Pulso(Rfil);
-      Pulso(RSalfil);
-      
-      Serial.print(arreg[f][c]);
-      Serial.print(", ");
-  }
-  
-  Serial.println();
-  delay(500);
-    
-  Pulso(RCol);
-  Pulso(Rsalcol);
-  }
-	*/
-  for(int x=0;x<8;x++){
-    for(int y = 0;y<8;y++){
-      digitalWrite(serFila,arreg[y][x]);
-      Pulso(Rfil);
-      Pulso(RSalfil);
-      
-    }
-  Pulso(RCol);
-  Pulso(Rsalcol);
+  Serial.println("Cuantos patrones desea mostrar");
+  p = IngresarDa();
 
-  }
 }
+ 
+void imagen(int *arr){
+    int i=0;//Columnas
+    int j=0;//Filas
+    int b=0;//Numero para sacar cada numero como enteros
+    int Col=1,Num=0,Div=0,Conter=1,Pos=0,Fin=10;//Variables para
+  	Serial.println("ingrese las posiciones que quiera prender de la columna de forma ascendente ");  
+  	while (Col!=9){
+      
+      Serial.print(Col);
+      Serial.print("-> ");
+      Num = IngresarDa();
+      Serial.println(Num);
+      for(b=10;;b=b*10){
+      	Div=Num/b;  
+      	if (Div<1){break;}//Ciclo para saber cuantos numeros ingreso                  
+      	Conter++;  
+      }
+      b=b/10;
+    while (Conter>0){
+            Pos=Num/b;
+            Pos=Pos%Fin;
+            while (Pos!=0){
+                i=Pos;
+                i=i-1;
+                *(arr+(8*j)+i)=1;
+                Pos=0;
+                Conter--;
+                b=b/10;
+            }
+
+            }
+            j++;
+            Col++;
+            i=0;
+            Conter=1;
+    }
+}
+
+//Funciones Auxiliares
+
 void Pulso(int pin){
   digitalWrite(pin,0);
   digitalWrite(pin,1);
   digitalWrite(pin,0); 
+}
+
+void iMPatron(int arreg[8][8]){
+  for(int x=0;x<8;x++){
+    for(int y = 0;y<8;y++){
+      Serial.print(arreg[x][y]);
+      digitalWrite(serFila,arreg[y][x]);
+      Pulso(Rfil);
+      Pulso(RSalfil);
+    }
+  Serial.println();
+  Pulso(RCol);
+  Pulso(Rsalcol);
+  }
+}
+
+
+int IngresarDa(){
+  int ent;
+  while(true){
+  if(Serial.available()>0){
+  	ent= Serial.parseInt();
+    break;
+  }}
+  return ent;
 }
